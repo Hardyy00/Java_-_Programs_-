b6ce7b3d208bd319a344d6250a81100619b112d4
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-06-20 20:54:22 +0530

    Delete challenge4.java

--- challenge4.java
+++
@@ -1,7 +0,0 @@
-public class challenge4 {
-    public static void main(String[] args){
-        double milesDriven = 19.89d;          // naming conventions
-        int customersCount = 56 ;
-        char lastKey = 'D';
-    }
-}
