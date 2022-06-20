8a5e8f339abf7d7dc5b2414232bfd984b1f23c87
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-06-20 21:18:41 +0530

    Update challenge6.java

--- Java_Masterclass/challenge6.java
+++ Java_Masterclass/challenge6.java
@@ -1,13 +1,13 @@
 public class challenge6 {
     public static int calculateHighScore(int score){
-        if(score >=1000)
+        if(score >=1000) {
             return 1;
-        else if(score >=500 && score <1000)
-            return 2 ;
-        else if(score >=100 && score <500)
+        }else if(score >=500 && score <1000) {
+            return 2;
+        }else if(score >=100 && score <500) {
             return 3;
-        else
-            return 4;
+        }
+        return 4;
     }
     public static void displayHighScore(String name,int highScorePosition){
         System.out.println(name + " managed to get position " +
@@ -15,6 +15,7 @@ public class challenge6 {
     }
 
     public static void main(String[] args){
+
         displayHighScore("Tim",calculateHighScore(1500));
 
         displayHighScore("Shane",calculateHighScore(900));
@@ -22,6 +23,8 @@ public class challenge6 {
         displayHighScore("Hardik",calculateHighScore(400));
 
         displayHighScore("Sara",calculateHighScore(50));
+
+
 
     }
 }
