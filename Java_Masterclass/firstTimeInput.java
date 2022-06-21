02f6176fa04bb8520785a6791249dbc1983d515a
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-06-21 22:51:35 +0530

    Add files via upload

---
+++ Java_Masterclass/firstTimeInput.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class firstTimeInput {
+    public static void main(String[] args){
+        Scanner cin = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int a = cin.nextInt();
+        System.out.println("Your input is " + a);
+    }
+}
