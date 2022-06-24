3383e37047bfbd0aa195d54c8abe9665aed3ff3e
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-06-24 09:55:57 +0530

    Add files via upload

---
+++ Java_Masterclass/challenge11.java
@@ -0,0 +1,13 @@
+public class challenge11 {
+    public static double calculateInterest(double amount,double interestRate){
+        return (amount*(interestRate/100));
+    }
+
+    public static void main(String[] args){
+        for(int i=2;i<9;i++){
+            System.out.println("10000 at " + i + "% interest = " 
+                                + calculateInterest(10000,i) );
+
+        }
+    }
+}
