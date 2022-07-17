2ddae23b03290d2895b869b27fae318533bd9afb
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-17 16:31:15 +0530

    Add files via upload

---
+++ Java_Masterclass/Challenge29/Song.java
@@ -0,0 +1,26 @@
+package Challenge29;
+
+public class Song {
+
+    private String songName;
+    private String songDuration;
+
+    public Song(String songName , String songDuration){
+
+        this.songName = songName;
+        this.songDuration = songDuration;
+    }
+
+    public String getSongName(){
+        return songName;
+    }
+
+    public String getSongDuration(){
+        return songDuration;
+    }
+
+    public static Song makeSong(String songName , String songDuration){
+
+        return new Song(songName , songDuration);
+    }
+}
