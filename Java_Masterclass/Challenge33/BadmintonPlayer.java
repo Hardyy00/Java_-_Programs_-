468bb17fc842635a96ee2d37f1b8e0ccf2360051
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-29 22:43:15 +0530

    Add files via upload

---
+++ Java_Masterclass/Challenge33/BadmintonPlayer.java
@@ -0,0 +1,11 @@
+package Challenge33;
+
+// This code is used to demonstrate the use of Generics in Java
+
+public class BadmintonPlayer extends Player{
+
+    public BadmintonPlayer(String name){
+        super(name);
+    }
+
+}
