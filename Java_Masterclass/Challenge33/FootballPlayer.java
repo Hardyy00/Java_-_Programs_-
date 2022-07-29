f0a7c5c8f8b4687620bf12724c116debb948bb96
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-29 22:44:04 +0530

    Add files via upload

---
+++ Java_Masterclass/Challenge33/FootballPlayer.java
@@ -0,0 +1,10 @@
+package Challenge33;
+
+// This code is used to demonstrate the use of Generics in Java
+
+public class FootballPlayer extends Player{
+
+    public FootballPlayer(String name){
+        super(name);
+    }
+}
