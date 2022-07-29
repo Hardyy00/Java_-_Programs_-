8bc8ede46dda25b69aa145b41321cf0e914c80bc
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-29 22:43:39 +0530

    Add files via upload

---
+++ Java_Masterclass/Challenge33/CricketPlayer.java
@@ -0,0 +1,10 @@
+package Challenge33;
+
+// This code is used to demonstrate the use of Generics in Java
+
+public class CricketPlayer extends Player{
+
+    public CricketPlayer(String name){
+        super(name);
+    }
+}
