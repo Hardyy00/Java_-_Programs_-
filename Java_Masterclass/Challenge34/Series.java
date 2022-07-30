7ff5e0a350a550164b0e5591b9150e6d0f3113db
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-30 20:15:48 +0530

    Add files via upload

---
+++ Java_Masterclass/Challenge34/Series.java
@@ -0,0 +1,43 @@
+package com.hardik;
+
+//This code is used to demonstrate Packaging
+
+/**
+ This Class had been converted into JAR file.
+ Artifact was also built.
+ And then JAR file was included into the project
+ through the library.
+ */
+
+public class Series {
+
+    public static int nSum(int n){
+
+        int sum = 0;
+        for(int i=1 ; i<=n; i++){
+            sum+= i;
+        }
+
+        return sum;
+    }
+
+    public static int factorial(int n){
+
+        int factorial = 1;
+
+        for(int i= 1; i<=n; i++ ){
+            factorial*= i;
+        }
+
+        return factorial;
+    }
+
+    public static int fibonacci(int n){
+
+        if(n>=0 && n<=1){
+            return n;
+        }
+
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
