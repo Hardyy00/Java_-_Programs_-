c2640ca0017200d0a09db700045c6f763ab881f7
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-08-26 08:38:05 +0530

    Add files via upload

---
+++ Java_Masterclass/Challenge37/Moon.java
@@ -0,0 +1,10 @@
+package challenge37.com.hardik;
+
+// Heavenly Body Class with more functionality
+
+public class Moon extends HeavenlyBody{
+
+    public Moon(String name, int orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.MOON);
+    }
+}
