59741f49093361853e4a699da451290626a15fb6
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-08-26 08:37:04 +0530

    Add files via upload

---
+++ Java_Masterclass/Challenge37/DwarfPlanet.java
@@ -0,0 +1,11 @@
+package challenge37.com.hardik;
+
+// Heavenly Body Class with more functionality
+
+public class DwarfPlanet extends HeavenlyBody{
+
+    public DwarfPlanet(String name, int orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.DWARF_PLANET);
+    }
+
+}
