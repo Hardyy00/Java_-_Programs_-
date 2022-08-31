fc403986eca6c2303214a4fc8b0531fe2895d869
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-08-31 21:31:05 +0530

    Add files via upload

---
+++ Koffmann_Book/Chapter_1/Question_4/SavingAccount.java
@@ -0,0 +1,36 @@
+package question4.com.hardik;
+
+public class SavingAccount extends Account{
+
+    public SavingAccount(Customer customer, long accountNumber) {
+        super(customer, accountNumber);
+    }
+
+    private void addTransaction(Transaction transaction){
+
+        if(super.getIncrementer() < 20){
+            super.reallocate();
+        }
+
+        super.transactions[getIncrementer()] = transaction;
+        setIncrementer();
+    }
+
+    public void depositMoney(double money){
+        addTransaction(new Transaction(money));
+        System.out.println("Money Deposited Successfully !");
+    }
+
+    public void withdrawMoney(double money){
+        addTransaction(new Transaction(-money));
+        System.out.println("Money withdrawn Successfully !");
+    }
+
+    public void addInterest(double interest){
+
+        double balance = getBalance();
+        setBalance(balance - (balance*0.06));
+        System.out.println("Interest Added Successfully");
+
+    }
+}
