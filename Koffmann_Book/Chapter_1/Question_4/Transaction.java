058a055ef1fd2bd7ff1f3a9547ddf850138700c0
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-08-31 21:31:23 +0530

    Add files via upload

---
+++ Koffmann_Book/Chapter_1/Question_4/Transaction.java
@@ -0,0 +1,14 @@
+package question4.com.hardik;
+
+public class Transaction {
+
+    private final double transaction;
+
+    public Transaction(double transaction){
+        this.transaction = transaction;
+    }
+
+    public double getTransaction(){
+        return transaction;
+    }
+}
